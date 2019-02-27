72a6dae4113270d8b62fa00c52b428723bf8dc91
Bluewind12 <zero18030@gmail.com>
2019-02-27 23:07:31 +0900

    テスト用レイアウトの作成

--- app/src/main/java/momonyan/recordapplication/MainActivity.kt
+++ app/src/main/java/momonyan/recordapplication/MainActivity.kt
@@ -1,12 +1,17 @@
-package momonyan.recordapplication
+package momonyan.recordapplication.Database
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.persistence.room.Room
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import momonyan.recordapplication.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(applicationContext, AppDataBase::class.java, "TestDataBase").build()
+
     }
 }
