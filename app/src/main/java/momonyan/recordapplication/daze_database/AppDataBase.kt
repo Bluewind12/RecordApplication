f297d9fb449b0af85b06a916b0042746dd3fe633
Bluewind12 <zero18030@gmail.com>
2019-04-05 22:55:30 +0900

    ワーニング回避。 room.schemaLocationのワーニングをなくす スキームを行わないようにした。

--- app/src/main/java/momonyan/recordapplication/daze_database/AppDataBase.kt
+++ app/src/main/java/momonyan/recordapplication/daze_database/AppDataBase.kt
@@ -3,7 +3,11 @@ package momonyan.recordapplication.daze_database
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 
-@Database(entities = [User::class], version = 2) // Kotlin 1.2からは arrayOf(Memo::class)の代わりに[Memo::class]と書ける
+@Database(
+    entities = [User::class],
+    version = 2,
+    exportSchema = false
+) // Kotlin 1.2からは arrayOf(Memo::class)の代わりに[Memo::class]と書ける
 abstract class AppDataBase : RoomDatabase() {
     // DAOを取得する。
     abstract fun userDao(): UserDao
